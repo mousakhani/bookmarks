865125fd2f1ab8099c307fe9b684bff20e1db977
khodemousa@gmail.com <khodemousa@gmail.com>
2021-08-29 23:04:54 +0430

    Register added

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from account.views import user_login, dashboard, home, user_logout
+from account.views import user_login, dashboard, home, user_logout, register
 
 app_name = 'account'
 urlpatterns = [
 	path('', home, name='home'),
+	path('register', register, name='register'),
 	path('login/', user_login, name='user_login'),
 	path('logout/', user_logout, name='logout'),
 	path('dashboard/', dashboard, name='dashboard')
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -20,7 +21,7 @@ def user_login(request):
 			if user is not None:
 				if user.is_active:
 					login(request, user)
-					return redirect('account:dashboard')
+					return redirect('account:home')
 				else:
 					return HttpResponse('Disabled account')
 			else:
@@ -39,3 +40,15 @@ def user_logout(request):
 @login_required()
 def dashboard(request):
 	return render(request, 'account/dashboard.html', {'dashboard': 'dashboard'})
+
+
+def register(request):
+	if request.method == 'POST':
+		user_form = UserCreationForm(request.POST)
+		if user_form.is_valid():
+			new_user = user_form.save(commit=False)
+			new_user.set_password(user_form.cleaned_data['password2'])
+			new_user.save()
+			login(request, new_user)
+			return render(request, 'account/register.html', {})
+	return render(request, 'account/register.html', {'form': UserCreationForm()})
