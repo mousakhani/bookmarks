2028f886e77644bdf53c89966a79b5a5bbc4e89d
khodemousa@gmail.com <khodemousa@gmail.com>
2021-08-29 22:40:44 +0430

    dashboard added

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from account.views import user_login
+from account.views import user_login, dashboard, home, user_logout
 
 app_name = 'account'
 urlpatterns = [
-	path('', user_login, name='user_login')
+	path('', home, name='home'),
+	path('login/', user_login, name='user_login'),
+	path('logout/', user_logout, name='logout'),
+	path('dashboard/', dashboard, name='dashboard')
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,14 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from account.forms import LoginForm
+
+
+def home(request):
+	return render(request, 'account/home.html', {})
 
 
 def user_login(request):
@@ -12,14 +17,25 @@ def user_login(request):
 		if form.is_valid():
 			cd = form.cleaned_data
 			user = authenticate(request, username=cd['username'], password=cd['password'])
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				return HttpResponse(user.username + ' logged in successfully')
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return redirect('account:dashboard')
+				else:
+					return HttpResponse('Disabled account')
 			else:
-				return HttpResponse('Disabled account')
-		else:
-			return HttpResponse(' Username or Password is invalid')
+				return HttpResponse(' Username or Password is invalid')
 	else:
 		form = LoginForm()
+
 	return render(request, 'account/login.html', {'form': form})
+
+
+def user_logout(request):
+	logout(request)
+	return redirect('account:home')
+
+
+@login_required()
+def dashboard(request):
+	return render(request, 'account/dashboard.html', {'dashboard': 'dashboard'})
