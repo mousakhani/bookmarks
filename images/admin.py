925560ea9039edb930519f644cfd0d8f57371c95
khodemousa@gmail.com <khodemousa@gmail.com>
2021-08-30 17:36:57 +0430

    image model fixed

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from images.models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+	fields = ['title', 'slug', 'image', 'created']
+	list_filter = ['created', ]
+	readonly_fields = ['created']
--- images/models.py
+++ images/models.py
@@ -1,17 +1,24 @@
 from django.conf import settings
 from django.db import models
 
-
 # Create your models here.
+from social_core.utils import slugify
 
 
 class Image(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='images_created')
-	title = models.CharField()
+	title = models.CharField(max_length=50)
 	slug = models.SlugField()
+	url = models.URLField()
 	image = models.ImageField(upload_to='images/%y/%m/%d/')
 	description = models.TextField(blank=True)
 	created = models.DateField(auto_now_add=True, db_index=True)
+	users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.title)
+		super(Image, self).save()
 
 	def __str__(self):
 		return self.title
